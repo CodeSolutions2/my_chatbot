d4c6bb4d6dcd413995c1c85d42f11c41d6d904ec
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-06-24 11:05:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-import { run_backend_process } from "./library_to_run_GitHub_Actions.js";
+import { run_backend_process, GET_text_from_file_wo_auth_GitHub_RESTAPI } from "./library_to_run_GitHub_Actions.js";
 window.run_backend_process = run_backend_process;
+window.GET_text_from_file_wo_auth_GitHub_RESTAPI = GET_text_from_file_wo_auth_GitHub_RESTAPI;
