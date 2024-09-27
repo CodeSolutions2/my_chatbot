b6ddb5dc1b234a809919bfd4c6739cdddabeb3ee
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-09-27 17:56:50 +0200

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-import { run_backend_process, GET_text_from_file_wo_auth_GitHub_RESTAPI, decode_desalt } from "./library_to_run_GitHub_Actions.js";
-window.run_backend_process = run_backend_process;
-window.GET_text_from_file_wo_auth_GitHub_RESTAPI = GET_text_from_file_wo_auth_GitHub_RESTAPI;
-window.decode_desalt = decode_desalt;
